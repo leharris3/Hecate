bcbe91bb462473e984e27f74aef8d0856b3bf6e4
levlevi <levlevi@email.unc.edu>
2022-09-24 16:57:29 -0400

    Depth: 2. Functional. Play in command line.

--- main.py
+++ main.py
@@ -1,17 +1,73 @@
 import chess
 import random
 import os
+import sys
+import chess.svg
+from IPython.core.display import SVG
+
+
+PIECE_VALUES = {1: 1, 2: 3,
+                3: 3, 4: 5, 5: 8, 6: 0}
+NEUTRAL_EVAL = 0
+IS_TURN = True
+IS_NOT_TURN = True
 
 
 class Engine:
+
     def __init__(self, board: chess.Board):
-        self.name = "Hectate"
+        self.name = "Hecate"
         self.board = board
 
     def move(self):
-        move_list = list(self.board.legal_moves)
-        random_move = random.choice(move_list)
-        return random_move
+        return self.evaluate()
+
+    def evaluate(self):
+        legalMoves = list(self.board.legal_moves)
+        bestEval = NEUTRAL_EVAL
+        canidateMoves = legalMoves
+
+        def evaluate_r(oppBoard: chess.Board, move: chess.Move):
+            oppBoard.push(move)
+            oppMoves = list(oppBoard.legal_moves)
+            bestOppEval = NEUTRAL_EVAL
+
+            if oppBoard.is_checkmate():
+                return sys.maxsize
+            for oppMove in oppMoves:
+                eval = NEUTRAL_EVAL
+                if (oppBoard.piece_at(oppMove.to_square) != None):
+                    eval = PIECE_VALUES[oppBoard.piece_at(
+                        oppMove.to_square).piece_type]
+                    if eval > bestOppEval:
+                        bestOppEval = eval
+            return bestOppEval
+
+        for move in legalMoves:
+            eval = NEUTRAL_EVAL
+            self.board.push(move)
+            if self.board.is_checkmate():
+                return move
+            self.board.pop()
+            if (self.board.piece_at(move.to_square) != None):
+                eval = PIECE_VALUES[self.board.piece_at(
+                    move.to_square).piece_type]
+
+            fin = self.board.fen()
+            clone = chess.Board(fin)
+            clone.color = chess.BLACK
+
+            if move != None:
+                eval -= evaluate_r(clone, move)
+
+            if eval > bestEval:
+                canidateMoves = []
+                canidateMoves.append(move)
+                bestEval = eval
+            elif eval == bestEval:
+                canidateMoves.append(move)
+
+            return random.choice(canidateMoves)
 
 
 class Game:
@@ -31,6 +87,7 @@ class Game:
                 self.makeEngineMove()
                 self.printGame()
                 self.makeUserMove()
+        print("Game Over!")
 
     def chooseColor(self):
         if (input("Choose a Side... Black or White?: ") == "White"):
